ef22d4a3a24f64818049af6478c0279b98ad4d0f
Ruslan <konovalenkoruslan462@gmail.com>
2023-12-21 10:21:03 +0200

    add ol topics

---
+++ topic_03/task3.py
@@ -0,0 +1,20 @@
+dict = {'Konovalenko': 'Ivan', 'Petrenko': 'Petro', 'Sidorova': 'Sidor'}
+new_dict = {'Smith': 'John', 'Doe': 'Jane', 'Johnson': 'James'}
+
+dict.update({'Brown': 'Michael', 'Davis': 'David'})
+print(f"Result of method 'update': {dict}")
+
+del dict['Konovalenko']
+print(f"Result of method 'del': {dict}")
+
+dict.clear()
+print(f"Result of method 'clear': {dict}")
+
+keys = new_dict.keys()
+print(f"Result of method 'keys': {keys}")
+
+values = new_dict.values()
+print(f"Result of method 'values': {values}")
+
+items = new_dict.items()
+print(f"Result of method 'items': {items}")
---
+++ topic_06/operations.py
@@ -0,0 +1,12 @@
+from functions import *
+def get_operation(operation_name):
+    if operation_name == "+":
+        return add
+    elif operation_name == "-":
+        return min
+    elif operation_name == "*":
+        return mult
+    elif operation_name == "/":
+        return div
+    else:
+        print("Некоректний ввід")
